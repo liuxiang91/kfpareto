bed480527d589b8f6af039a2408960e754847f5f
Xiang Liu <liuxiang@umich.edu>
2017-04-06 23:34:16 -0400

    status bar added, parfor loop moved to outer, added parfor constant to speedup

--- nonParetoAnalysis.m
+++ nonParetoAnalysis.m
@@ -3,23 +3,36 @@ function [ AccTest ,ddTest,NTTest] = nonParetoAnalysis(A,C,Q,R,INITX,INITV,o, tr
 %   Detailed explanation goes here
 close all;
 n=length(0:step:1);
-rhos=0:step:1;
-betas=0:step:1;
+rhos=parallel.pool.Constant(0:step:1);
+betas=parallel.pool.Constant(0:step:1);
 
-
+warning off
 
 
 
 accTNTmatT=nan(n,n);
 ddTNTmatT=nan(n,n);
 numTestPerYearTNTmatT=nan(n,n);
+
+A=parallel.pool.Constant(A);
+C=parallel.pool.Constant(C);
+Q=parallel.pool.Constant(Q);
+R=parallel.pool.Constant(R);
+INITX=parallel.pool.Constant(INITX);
+INITV=parallel.pool.Constant(INITV);
+o=parallel.pool.Constant(o);
+test=parallel.pool.Constant(test);
+fprintf('Running rho-beta combinations:\n')
+parfor_progress(n);
+
 parfor i=1:n
     for j=1:n
-            fprintf('Test i=%d,j=%d\n',i,j);
-	[accTNTmatT(i,j),d1,numTestPerYearTNTmatT(i,j),~]=TNT(A,C,Q,R,INITV,INITX,o,betas(i),rhos(j),test);
-            ddTNTmatT(i,j)=mean(d1);
-        end
+		[accTNTmatT(i,j),d1,numTestPerYearTNTmatT(i,j),~]=TNT(A.Value,C.Value,Q.Value,R.Value,INITV.Value,INITX.Value,o.Value,betas.Value(i),rhos.Value(j),test.Value);
+        ddTNTmatT(i,j)=mean(d1);
     end
+    parfor_progress;
+end
+parfor_progress(0);
 
 
 AccTest=accTNTmatT(:);
